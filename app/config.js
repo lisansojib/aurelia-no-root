f615ba37485a9326dbd5f39bbe819fb12895b282
MD. SOJIB MIYA <md.sojibmiya@gmail.com>
2019-01-20 21:59:21 +0600

    fastclick and jquery-slimscrool packages added

--- app/config.js
+++ app/config.js
@@ -24,9 +24,11 @@ System.config({
     "aurelia-templating-router": "npm:aurelia-templating-router@1.0.0",
     "bluebird": "npm:bluebird@3.4.1",
     "bootstrap": "github:twbs/bootstrap@3.3.7",
+    "fastclick": "npm:fastclick@1.0.6",
     "fetch": "github:github/fetch@1.0.0",
     "font-awesome": "npm:font-awesome@4.6.3",
     "jquery": "npm:jquery@2.2.4",
+    "jquery-slimscroll": "npm:jquery-slimscroll@1.3.8",
     "text": "github:systemjs/plugin-text@0.0.8",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
@@ -208,6 +210,10 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:jquery-slimscroll@1.3.8": {
+      "jquery": "npm:jquery@2.2.4",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:jwt-decode@2.2.0": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
