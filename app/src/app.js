d80a57c74995c4f84c989059b813ef9a873a685e
MD. SOJIB MIYA <md.sojibmiya@gmail.com>
2019-01-21 06:37:22 +0600

    adminlte functionalities implemented

--- app/src/app.js
+++ app/src/app.js
@@ -1,12 +1,21 @@
+import { inject } from 'aurelia-framework';
+
 export class App {
+  constructor() {
+  }
+
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'AdminLTE 2 | Fixed Layout';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'login'], name: 'login', moduleId: 'auth/login', nav: true, title: 'Login' },
+      { route: 'welcome', name: 'welcome', moduleId: 'welcome', nav: true },
+      { route: 'register', name: 'register', moduleId: 'auth/register', nav: true, title: 'Register' }
     ]);
 
     this.router = router;
+  }
+
+  attached() {
+    $('body').layout('fix')
   }
 }
